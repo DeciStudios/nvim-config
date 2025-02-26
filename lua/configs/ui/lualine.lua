798f2d979fda89267d2afb2a1e4cca994f4391f5
Jack Mortimer <jackmortimer33@gmail.com>
2025-02-26 00:21:14 +0000

    Switched to rounded lualine

--- lua/configs/ui/lualine.lua
+++ lua/configs/ui/lualine.lua
@@ -1,66 +1,66 @@
 local seperatorTypes = {
-    default = {
-        section = { left = '', right = '' },
-        component = { left = '', right = '' }
-    },
-    slant = {
-        section = { left = '', right = '' },
-        component = { left = '', right = '' }
-    },
-    arrow = {
-        section = { left = '', right = '' },
-        component = { left = '', right = '' }
-    },
-    curve = {
-        section = { left = '', right = '' },
-        component = { left = '', right = '' }
-    },
-    none = {
-        section = { left = '', right = '' },
-        component = { left = '', right = '' }
-    }
-
+	default = {
+		section = { left = "", right = "" },
+		component = { left = "", right = "" },
+	},
+	slant = {
+		section = { left = "", right = "" },
+		component = { left = "", right = "" },
+	},
+	arrow = {
+		section = { left = "", right = "" },
+		component = { left = "", right = "" },
+	},
+	curve = {
+		section = { left = "", right = "" },
+		component = { left = "", right = "" },
+	},
+	none = {
+		section = { left = "", right = "" },
+		component = { left = "", right = "" },
+	},
 }
 
 local M = {
-    options = {
-        icons_enabled = false,
-        component_separators = seperatorTypes.none.component,
-        section_separators = seperatorTypes.none.section,
-        globalstatus = true, -- Add this line
-    },
-    sections = {
-        lualine_a = { {
-            'mode',
-            -- fmt = function(str)
-            --     if str == "NORMAL" then
-            --         return ' ' .. str
-            --     elseif str == "INSERT" then
-            --         return ' ' .. str
-            --     elseif str == "VISUAL" then
-            --         return '󰈈 ' .. str
-            --     elseif str == "V-LINE" then
-            --         return ' ' .. str
-            --     end
-            -- end
-        } -- Add icon before the mode text
-        },
-        lualine_b = {
-            "branch",
-        },
-        lualine_c = {
-            'filename',
-            --function()
-            --    return vim.fn['nvim_treesitter#statusline'](180)
-            --end
-        },
-        lualine_x = { 'copilot' },
-        lualine_y = { 'location' },
-        lualine_z = { 'filetype' }
-    },
-    tabline = {},
-    extensions = {},
-    winbar = nil,
+	options = {
+		icons_enabled = false,
+		component_separators = seperatorTypes.slant.component,
+		section_separators = seperatorTypes.slant.section,
+		globalstatus = true, -- Add this line
+	},
+	sections = {
+		lualine_a = {
+			{
+				"mode",
+				-- fmt = function(str)
+				--     if str == "NORMAL" then
+				--         return ' ' .. str
+				--     elseif str == "INSERT" then
+				--         return ' ' .. str
+				--     elseif str == "VISUAL" then
+				--         return '󰈈 ' .. str
+				--     elseif str == "V-LINE" then
+				--         return ' ' .. str
+				--     end
+				-- end
+			}, -- Add icon before the mode text
+		},
+		lualine_b = {
+			"branch",
+		},
+		lualine_c = {
+			"filename",
+			--function()
+			--    return vim.fn['nvim_treesitter#statusline'](180)
+			--end
+		},
+		lualine_x = { "copilot" },
+		lualine_y = { "location" },
+		lualine_z = { "filetype" },
+	},
+	tabline = {},
+	extensions = {},
+	winbar = nil,
 }
 
 return M
